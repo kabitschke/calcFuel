81aa88b5fb46a1ff8618465c51e0ccd9bbe60df4
Maycon Kabitschke <mayconkabitschke@gmail.com>
2024-10-12 02:06:35 -0300

    ajuste menu

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,63 +4,60 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useState } from "react";
 
 export const Header = () => {
-
   const [showMenu, setShowMenu] = useState(false);
 
-    return(
-        <div className=" bg-blue-600 w-full  p-4 text-white">
-          <div className="container flex justify-between items-center mx-auto">
-            
+  return (
+    <>
+      <div className="bg-blue-600 w-full p-4 text-white">
+        <div className="container flex justify-between items-center mx-auto">
+          <div className="hidden md:block">
+            <FontAwesomeIcon icon={faGasPump} className="size-6" />
+          </div>
 
-            <div className="hidden md:block">
-              <FontAwesomeIcon icon={faGasPump} className="size-6"/>
-            </div>
+          <div className="text-2xl font-medium">Cálculo de combustível</div>
 
-            <div className="text-2xl font-medium">
-                    Cálculo de combustível 
+          <nav>
+            <ul>
+              <div className={`sm:hidden cursor-pointer`} onClick={() => setShowMenu(!showMenu)}>
+                {showMenu ? (
+                  <FontAwesomeIcon icon={faXmark} className="size-6" />
+                ) : (
+                  <FontAwesomeIcon icon={faBars} className="size-6" />
+                )}
+              </div>
 
-            </div>
+              <div className="hidden sm:flex sm:gap-4">
+                <ul className="flex gap-4">
+                  <li><a href="#home" className="hover:text-blue-300 hover:border-b-blue-300 hover:border-b">Home</a></li>
+                  <li><a href="#calc1" className="hover:text-blue-300 hover:border-b-blue-300 hover:border-b">Calculadora</a></li>
+                  <li><a href="#calc2" className="hover:text-blue-300 hover:border-b-blue-300 hover:border-b">Álcool/Gasolina</a></li>
+                </ul>
+              </div>
 
-            <nav>
-              <ul className="">
-                <div className={`sm:hidden cursor-pointer `} onClick={() => setShowMenu(!showMenu)}>
-                  {
-                    !showMenu &&
-                    <FontAwesomeIcon icon={faBars} className="size-6" />
-                  }
-
-                  {
-                     showMenu &&
-
-                     <div className="flex gap-4">
-                        <div className="">
-                        <FontAwesomeIcon icon={faXmark} className="size-6" />
-                        </div>
-
-                        
-                          <div className="">
-                          <li><a href="#home" className="hover:text-blue-300">Home</a></li>
-                          <li><a href="#calc1" className="hover:text-blue-300">Calculadora</a></li>
-                          <li><a href="#calc2" className="hover:text-blue-300">Álcool/Gasolina</a></li>
-                                 
-                          </div>
-
-                      </div>
-                  }
-
-                </div>
-
-                <div className="hidden sm:flex sm:gap-4 ">
-                  <li><a href="#home" className="hover:text-blue-300">Home</a></li>
-                  <li><a href="#calc1" className="hover:text-blue-300">Calculadora</a></li>
-                  <li><a href="#calc2" className="hover:text-blue-300">Álcool/Gasolina</a></li>
-                </div>          
-            
-              </ul>
-            </nav>
-
-        </div>  
-    
+            </ul>
+          </nav>
+        </div>
       </div>
-    );
+
+   
+      {showMenu && (
+        <div className="lg:hidden md:hidden bg-blue-600 p-4 rounded-md mt-1 text-white  mx-1">
+          <ul>
+            <li className="border-b border-b-blue-500">
+              <a href="#home" className="hover:text-blue-300">Home</a>
+            </li>
+            <li className="border-b border-b-blue-500">
+              <a href="#calc1" className="hover:text-blue-300">Calculadora</a>
+            </li>
+            <li className="border-b border-b-blue-500">
+              <a href="#calc2" className="hover:text-blue-300">Álcool/Gasolina</a>
+            </li>
+          </ul>
+        </div>
+      )}
+
+   
+     
+    </>
+  );
 }
